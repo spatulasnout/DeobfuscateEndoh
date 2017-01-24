5f889a649b4c2563baa0f4429c318cfe3b17c171
spatulasnout <spatulasnout@cila-search.net>
2017-01-24 00:52:04 -0800

    Converted from std::complex to 2D vector.

--- endoh1_deobfuscate.cpp
+++ endoh1_deobfuscate.cpp
@@ -1,6 +1,22 @@
+//
+// Original: http://www.ioccc.org/2012/endoh1/hint.html
+//
+// Initial deobfuscation by: https://github.com/litonico/DeobfuscateEndoh
+//
+// Continued deobfuscation by: https://github.com/spatulasnout/DeobfuscateEndoh
+//	- Converted to C++ because my C compiler has no "double complex" support.
+//	- Separated out init / run methods.
+//	- Deobfuscated remaining tricky one-liners.
+//	- Converted to Particle struct.
+//	- Converted to 2D vector instead of std::complex.
+//
+
+#define USE_COMPLEX_ABS_PRECISION	0	// see Vector2D::length for explanation
 
 #include <exception>
+#if USE_COMPLEX_ABS_PRECISION
 #include <complex>
+#endif
 #include <memory>
 #include <chrono>
 #include <thread>
@@ -10,23 +26,140 @@
 #include "ansicolor-w32.h"
 #endif
 
-
+#if USE_COMPLEX_ABS_PRECISION
 using std::complex;
+#endif
 using std::unique_ptr;
 
 #define Gravity		1.0f
 #define Pressure	4.0f
 #define Velocity	8.0f
 
+template <typename T>
+struct Vector2D {
+	typedef T scalar_type;
+	typedef Vector2D<T> this_type;
+
+	union {
+		scalar_type		v[2];
+		struct {
+			scalar_type	x;
+			scalar_type	y;
+		};
+	};
+
+	Vector2D () : x(0), y(0) {}
+	Vector2D (scalar_type x_, scalar_type y_) : x(x_), y(y_) {}
+
+	inline void set (scalar_type x_, scalar_type y_) { x=x_; y=y_; }
+	inline void zero () { x = 0; y = 0; }
+
+	inline scalar_type	operator[] (int idx) const { return v[idx]; }
+	inline scalar_type & operator[] (int idx) { return v[idx]; }
+
+	inline this_type & operator= (const this_type &other)
+	{
+		x = other.x;
+		y = other.y;
+		return (*this);
+	}
+
+	inline bool operator== (const this_type &other) const
+	{
+		return x == other.x  &&  y == other.y;
+	}
+
+	inline bool operator!= (const this_type &other) const
+	{
+		return ! ((*this) == other);
+	}
+
+	inline this_type & operator+=(const this_type &other)
+	{
+		x += other.x;
+		y += other.y;
+		return *this;
+	}
+
+	inline this_type & operator-=(const this_type &other)
+	{
+		x -= other.x;
+		y -= other.y;
+		return *this;
+	}
+
+	inline this_type & operator*=(const scalar_type factor)
+	{
+		x *= factor;
+		y *= factor;
+		return *this;
+	}
+
+	inline this_type & operator/=(const scalar_type factor)
+	{
+		x /= factor;
+		y /= factor;
+		return *this;
+	}
+
+	inline this_type operator+(const this_type &other) const
+	{
+		this_type tmp(*this);
+		return tmp += other;
+	}
+
+	inline this_type operator-(const this_type &other) const
+	{
+		this_type tmp(*this);
+		return tmp -= other;
+	}
+
+	inline this_type operator*(const scalar_type factor) const
+	{
+		this_type tmp(*this);
+		return tmp *= factor;
+	}
+
+	inline this_type operator/(const scalar_type factor) const
+	{
+		this_type tmp(*this);
+		return tmp /= factor;
+	}
+
+	inline scalar_type length () const
+	{
+		// NOTE: abs(complex&) (at least the MSVC++ implementation) goes
+		// through some lengths to preserve precision.  In order to achieve
+		// identical behavior to the original, we need to use abs(complex&)
+		// here to compute length.
+		//
+		// However, using the simpler std::sqrt-based length forumla runs
+		// significantly faster--speeds up framerate by over 2x--and produces
+		// almost the same output (very slight differences over time.)
+#if USE_COMPLEX_ABS_PRECISION
+		complex<scalar_type> cv(x, y);
+		return std::abs(cv);
+#else
+		return std::sqrt(x*x + y*y);
+#endif
+	}
+};
+
+template <typename T>
+inline Vector2D<T> operator*(const T factor, const Vector2D<T> &rhs)
+{
+	return rhs * factor;
+}
+
 typedef float real_type;
-typedef complex<real_type> complex_type;
+typedef Vector2D<real_type> vec2_type;
 
 struct Particle {
-	complex_type	position;
-	int				wall_flag;
-	real_type		density;
-	complex_type	force;
-	complex_type	velocity;
+	vec2_type	position;
+	vec2_type	force;
+	vec2_type	velocity;
+	real_type	density;
+	int			wall_flag;
 };
 
 class EndohDeobfuscate {
@@ -34,13 +167,11 @@ public:
 	static const size_t SIMBUF_LEN = 19538;
 	static const size_t SCRBUF_LEN = 6856;
 	static const int SCR_WID = 80;
+	static const int SCR_HGT = 24;
 
 	void init (FILE *in = stdin)
 	{
-		using namespace std::complex_literals;
-
-		// const complex_type I(1i);
-		complex_type w;
+		vec2_type w;
 
 		r = a;
 		for (int ch;  (ch = getc(in)) != EOF; ) {
@@ -49,26 +180,22 @@ public:
 				if (ch > ' ') {
 					Particle *s = (r + 1);
 					r->position = w;
-					s->position = (w + 1.0f);
+					s->position = vec2_type(w.x, w.y + 1.0f);
 					r->wall_flag = s->wall_flag = (ch == '#');
 					r += 2;
 				}
 
-				// w = w - I;
-				w.imag( w.imag() + 1.0f );
+
+				w.x += 1.0f;
 			}
 			else {
-				w = complex_type(((int)w.real()) + 2, 0);
+				w = vec2_type(0, ((int)w.y) + 2);
 			}
 		}
 	}
 
 	void run ()
 	{
-		using namespace std::complex_literals;
-
-		// const complex_type I(1i);
-
 		memset(b, 0, SCRBUF_LEN);
 
 		clearscreen();
@@ -80,7 +207,7 @@ public:
 
 		for (;;) {
 			Particle *p, *q;
-			complex_type d;
+			vec2_type d;
 			real_type w;
 			int x, y;
 			char *t;
@@ -101,7 +228,7 @@ public:
 			for (p = a; p < r; ++p) {
 				p->density = p->wall_flag * 9.0f;
 				for (q = a; q < r; ++q) {
-					w = abs(d = p->position - q->position) / 2 - 1;
+					w = (d = p->position - q->position).length() / 2 - 1;
 					if (0 < (x = (1.0f - w))) {
 						p->density += w * w;
 						++dbg__sim_ops_per_frame;
@@ -111,11 +238,11 @@ public:
 			}
 
 			for (p = a; p < r; ++p) {
-				p->force = Gravity;
+				p->force = vec2_type(0, Gravity);
 				for (q = a; q < r; ++q) {
-					w = abs(d = p->position - q->position) / 2 - 1;
+					w = (d = p->position - q->position).length() / 2 - 1;
 					if (0 < (x = (1.0f - w))) {
-						p->force += w * ((d * (3.0f - p->density - q->density) * Pressure) + (p->velocity * Velocity) - (q->velocity * Velocity)) / p->density;
+						p->force += (w * ((d * (3.0f - p->density - q->density) * Pressure) + (p->velocity * Velocity) - (q->velocity * Velocity))) / p->density;
 						++dbg__sim_ops_per_frame;
 					}
 					++dbg__sim_cells_per_frame;
@@ -129,9 +256,9 @@ public:
 
 			// compute marching square edges, and sim cell position update
 			for (p = a; p < r; ++p) {
-				t = b + (x = ( p->position.imag())) + SCR_WID * (y = (p->position.real() / 2.0f));
+				t = b + (x = (p->position.x)) + SCR_WID * (y = (p->position.y / 2.0f));
 				p->position += p->velocity += (p->force / 10.0f) * (float)(!(p->wall_flag));
-				if (0 <= x && x < 79 && 0 <= y && y < 23) {
+				if (0 <= x && x < (SCR_WID - 1) && 0 <= y && y < (SCR_HGT - 1)) {
 					t[0] |= 8;
 					t[1] |= 4;
 					t += SCR_WID;
@@ -176,6 +303,10 @@ protected:
 };
 
 
+// Expects a text document providing initial state on stdin.
+// For example, feed it the original "endoh1.c" source code
+// which looks like the word "Fluid".
+
 int main (int argc, char **argv)
 {
 	try {
@@ -196,187 +327,10 @@ int main (int argc, char **argv)
 
 
 
-
-
-
-
-
-
-
 #if 0
 
-// Re-assignment:
-// G -> Gravity
-// P -> Pressure
-// V -> Velocity
-// These are read in from command line args
-
-// Endoh:
-// "Though some assignments may look meaningless, it is actually meaningful;
-// it extracts “integer part of real part” from a complex value by assigning
-// (and casting) it to an integer-type variable."
-
-// Essential info: in C, a = b EVALUATES to 0, and performs an assignment
-// as a 'side effect.' Assignment in the initialization of a for loop will
-// make the compiler complain; when he uses this, Mr. Endoh wraps the
-// expressions in parens to silence that.
-
-
-// Use of the comma operator in C:
-// For example, in the statement
-// *r++ = w, r
-//
-// The contents of the address pointed to by r is set to w (a complex double).
-// The assignment produces a return value of 0, which the comma promptly discards.
-// r is returned (a pointer)
-// The contents of r are incremented (postfix operator)
-//
-// Mr. Endoh uses the comma extensively to perform assignment inline.
-
-#include <stdio.h>//  .IOCCC                                         Fluid-  #
-#include <unistd.h>  //2012                                         _Sim!_  #
-#include <complex.h>  //||||                     ,____.              IOCCC-  #
-
-double complex	a[97687],
-// Endoh: "Every five entries of double complex a[] contain information
-// of one particle: position, wall-flag, density, force, and velocity in turn."
-       *p, *q, *r = a, w = 0, d;
-
-int	x, y;
-char b[6856] = "\x1b[2J" //clears the screen (ANSI escape code). '\x1b' is equivalent to ESC.
-               "\x1b[1;1H     "; // Moves the cursor to line 1, char 1
-                                 //TODO: unclear if whitespace important
-
-char *o = b, *t;
-
-// The character # represents “wall particle” (a particle with fixed position),
-// and any other non-space characters represent free particles.
-
-int main(){
-
-    // Endoh: "This program reads a text from standard input, and uses it as 
-    // an initial configuration of the particles."
-	for (; 0 < (x = getc(stdin));){ // While the end of stdin is not reached
-         w = (x > 10 ? (32 < x ? 4[*r++ = w, r] = w + 1, *r = r[5] = x == 35, r += 9 : 0, w - _Complex_I) : (x = w + 2)); // why the heck were there two semicolons
-
-        // Breaking it down!
-        //
-        // 4[*r++ = w, r] = w + 1,
-        //
-        // 4[x] is a weird but valid array access: it's the same as x[4] 
-        // for any array x.  The expression x[y] is defined to be *((x)+(y)), 
-        // so x and y can be swapped (Kernigan and Ritchie).
-        //
-        // Again, the comma makes this into a multi-statement instruction
-        // *r++ = w; // -> returns 0, which is disregarded
-        // r[4] = w+1;
-        //
-        // Note the order of application in `*r++'! To be a little more clear:
-        //
-        // *(r++) = w;
-        // r[4] = w+1;
-        //
-        //
-        // Okay, next bit
-        // *r = r[5] = x == 35,
-        //
-        // Assignment is right-associative, so let's put some parens in
-        //
-        // (*r = (r[5] = (x == 35))),
-        //  
-        // Doesn't really look much better, does it? But we can break it down.
-        // x == 35 is a boolean: x is the current character, and 35 is the 
-        // ASCII code for '#' (the 'hash' symbol), which Mr. Endoh uses to mark
-        // a wall in the simulation.
-        //
-        // Let's rewrite this as an if statement:
-        //
-        // *r is r[0]; given the context, that's a little clearer
-        //
-        // if (x == 35) // if x (the current character) is '#'
-        //      // This sets the 'wall flag' for unmoveable particles.
-        //      r[0] = r[5] = 1;
-        // else 
-        //      r[0] = r[5] = 0;
-        //
-        //
-        // More verbose, but more comprehensible!
-        //
-        // r += 9;
-        //
-
-        /*
-        if (x > 10){
-            //  
-            w = (32 < x ? 4[*r++ = w, r] = w + 1, *r = r[5] = x == 35, r += 9 : 0, (w - _Complex_I));
-            
-            if (x > 32){
-                w = 4[*r++ = w, r] = w + 1, *r = r[5] = x == 35, r += 9;
-            }
-            else{
-                w = 0, w - _Complex_I;
-            }
-            //
-        }
-        else {
-            x = w + 2;
-            w = x;
-        }
-        
-        */
- 
-    }
-
-	for (;; puts(o), o = b + 4) { // The SPH algorithm
-        // while(true){ 
-        // puts(o) // clears the screen, returns 0 (probably- need to check the value of o)
-        // the comma discards the return value
-        // o = b + 4 // o points to the 4th elem of b
-
-		for (p = a; p[2] = p[1] * 9, p < r; p += 5){
-			for (q = a; w = cabs(d = *p - *q) / 2 - 1, q < r; q += 5){
-				if (0 < (x = 1 - w)){
-					p[2] += w * w;
-                }
-            }
-        }
-
-        // Iterate over the positions(?) of particles
-		for (p = a; p[3] = Gravity, p < r; p += 5){
-			for (q = a; w = cabs(d = *p - *q) / 2 - 1, q < r; q += 5){
-				if (0 < (x = 1 - w)){
-					p[3] += w * (d * (3 - p[2] - q[2]) * Pressure + p[4] * Velocity - q[4] * Velocity) / p[2];
-                }
-            }
-        }
-
-		for (x = 011; 2012 - 1 > x++;){ // 011 is ASCII horizontal tab in octal; important?
-			b[x] = 0;
-        }
-
-		for (p = a; (t = b + 10 + (x = *p * _Complex_I) + 80 * (y = *p / 2), *p += p[4] += p[3] / 10 * !p[1]), p < r; p += 5) {
-			x = 0 <= x && x < 79 && 0 <= y && y < 23 ? 1[1[*t |= 8, t] |= 4, t += 80] = 1, *t |= 2 : 0;
-        }
-
-		for (x = 011; 2012 - 1 > x++;){ // Render the particles as marching squares
-			b[x] = " '`-.|//,\\" "|\\_" "\\/\x23\n"[x % 80 - 9 ? x[b] : 16];; // \x23 is the # (hash) symbol in hex ASCII
-            // Maybe uses \x23 instead of # because # is the walls?
-            // Uses array offsets to get the appropriate set of chars for the 
-            // Marching squares algorithm
-            // 0 =  (space)
-            // 1 = '
-            // 2 = `
-            // 3 = - 
-            // 4 = 
-        }
-
-		usleep(12321);
-	} 
-    
-    return 0;
-}
-
 // ****************************************************************************
+// The original endoh1.c:
 
 #  include<stdio.h>//  .IOCCC                                         Fluid-  #
 #  include <unistd.h>  //2012                                         _Sim!_  #
@@ -403,64 +357,6 @@ int main(){
 **###########################################################################*/
 
 // ****************************************************************************
-
-double complex a [  97687]  ,*p,*q     ,*r=a,  w=0,d;
-int x,y;
-char b[6856]="\x1b[2J"  "\x1b"  "[1;1H     ", *o=  b, *t;
-
-int main ()
-{
-	for(; 0<( x = getc (stdin)  );)
-		w=x  >10?32<     x?4[
-		*r++              =w,r]=  w+1,*r     =r[5]=  x==35,  r+=9:0      ,w-I
-			:(x=              w+2);;
-			for(;;     puts(o  ),o=b+  4){
-			for( p=a; p [2]=p [1]* 9,p<r; p+=5)
-			for( q=a; w=cabs (d=*p- *q)/2- 1,q<r; q+=5)
-			if(0 <(x=1- w))p[2]+=w*              w;
-			for( p=a; p [3]=G,p<r; p+=5)
-			for( q=a; w=cabs (d=*p- *q)/2- 1,q<r; q+=5)
-			if(0 <(x=1- w))p[3]+=w*(d*(     3-p[2]  -q[2])  *P+p[4      ]*V-
-				q[4]              *V)/p[  2];
-			for( x=011; 2012 -1>x ++;)b[ x]=0     ;
-			for( p=a; ( t=b+10 +(x=*p *I)+ 80*( y=*p/2 ),*p+=p [4]+=p [3]/10 *!p[1]),p<r; p+=5)x=0
-				<=x              &&x<79   &&0<=y&&y<23?1[1  [*t|=8   ,t]|=4,t+=80]=1
-				, *t              |=2:0;
-				for( x=011; 2012 -1>x ++;)b[ x]=" '`-.|//,\\"  "|\\_"    "\\/\x23\n"[x
-				%80-              9?x[b]      :16];;usleep(  12321)      ;
-			}
-	return 0;
-}
-
-// ****************************************************************************
-
-double complex a [  97687], *p, *q, *r = a,  w = 0, d;
-int x, y;
-char b[6856] = "\x1b[2J"  "\x1b"  "[1;1H     ", *o =  b, *t;
-
-int main ()
-{
-	for (; 0 < ( x = getc (stdin)  );)
-		w = x  > 10 ? 32 <     x ? 4[		*r++              = w, r] =  w + 1, *r     = r[5] =  x == 35,  r += 9 : 0, w - I
-		: (x =              w + 2);;
-	for (;;     puts(o  ), o = b +  4) {
-		for ( p = a; p [2] = p [1] * 9, p < r; p += 5)
-			for ( q = a; w = cabs (d = *p - *q) / 2 - 1, q < r; q += 5)
-				if (0 < (x = 1 - w))p[2] += w *              w;
-		for ( p = a; p [3] = G, p < r; p += 5)
-			for ( q = a; w = cabs (d = *p - *q) / 2 - 1, q < r; q += 5)
-				if (0 < (x = 1 - w))
-					p[3] += w * (d * (     3 - p[2]  - q[2])  * P + p[4      ] * V -		q[4]              * V) / p[  2];
-		for ( x = 011; 2012 - 1 > x ++;)b[ x] = 0     ;
-		for ( p = a; ( t = b + 10 + (x = *p * I) + 80 * ( y = *p / 2 ), *p += p [4] += p [3] / 10 * !p[1]), p < r; p += 5)
-			x = 0	<= x              && x < 79   && 0 <= y && y < 23 ? 1[1  [*t |= 8, t] |= 4, t += 80] = 1
-			, *t              |= 2 : 0;
-		for ( x = 011; 2012 - 1 > x ++;)
-			b[ x] = " '`-.|//,\\"  "|\\_"    "\\/\x23\n"[x % 80 -              9 ? x[b]      : 16];;
-		usleep(  12321)      ;
-	}
-	return 0;
-}
 
 // gcc -DG=1 -DP=4 -DV=8 -D_BSD_SOURCE -std=c99 -Wall -W -Wextra -pedantic -O2 -o endoh1.exe endoh1.c
 // gcc -DGravity=1 -DPressure=4 -DVelocity=8 -D_BSD_SOURCE -std=c99 -Wall -W -Wextra -pedantic -O2 -o endoh1_deobfuscate.exe endoh1_deobfuscate.c
